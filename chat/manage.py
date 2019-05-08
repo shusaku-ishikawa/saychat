c40593d037b34e6da35b4d14e4712c285ac4338b
root <root@ik1-306-13474.vs.sakura.ne.jp>
2019-05-08 09:37:55 +0900

    a

--- chat/manage.py
+++ chat/manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-
+mport pymysql
+pymysql.install_as_MySQLdb()
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
     try:
